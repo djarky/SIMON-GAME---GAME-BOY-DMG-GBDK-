f1f1420c16c0e0f8d3cc85e7f455c21d93d355eb
djarky <aivr_95@hotmail.com>
2017-03-10 10:34:42 -0400

    Add files via upload

---
+++ 2.bat
@@ -0,0 +1,3 @@
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o maindk.o maindk.c
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt3 -Wl-yo4 -Wl-ya4 -Wl-yp0x143=0x00 -o RMG.gb maindk.o t.o
+pause
